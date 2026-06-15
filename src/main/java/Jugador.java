9e17baa80c8edcbbc7a503a613175e586c58a131
Matias Cabello <maticab170@gmail.com>
2026-06-15 17:40:13 -0400

    feat: Implementar codigo base en Participante, Jugador y Equipo

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,2 +1,16 @@
-public class Jugador {
+public class Jugador extends Participante{
+    private String rut;
+
+    public Jugador(String nombre, String contacto, String rut) {
+        super(nombre, contacto);
+        this.rut = rut;
+    }
+
+    public String getRut() { return rut; }
+    public void setRut(String rut) { this.rut = rut; }
+
+    @Override
+    public void verDetalles() {
+        System.out.println("Jugador: " + getNombre() + " (RUT: " + rut + ")");
+    }
 }
