d86588ac2b79fcd6e7be2396f615ed2b7f9192c7
Matias Cabello <maticab170@gmail.com>
2026-06-17 00:48:05 -0400

    feat: Implementar codigo base Partida y main para probar funcionamiento

--- src/main/java/Partida.java
+++ src/main/java/Partida.java
@@ -1,2 +1,56 @@
 public class Partida {
+    private Participante p1;
+    private Participante p2;
+    private int puntaje1;
+    private int puntaje2;
+    private Participante ganador;
+    private EstadoPartida estado;
+
+    public Partida(Participante p1,Participante p2){
+        this.p1 = p1;
+        this.p2 = p2;
+        estado = EstadoPartida.PENDIENTE;
+    }
+
+    public void registrarResultado(int puntaje1, int puntaje2){
+        this.puntaje1 = puntaje1;
+        this.puntaje2 = puntaje2;
+        estado = EstadoPartida.TERMINADA;
+
+        if (puntaje1 > puntaje2){
+            ganador = p1;
+        } else if (puntaje2 > puntaje1){
+            ganador = p2;
+        } else {
+            ganador = null;
+        }
+
+        if (ganador != null) {
+            System.out.println("Termino partido. \nPuntaje: " + getPuntaje1()+ " a " +getPuntaje2()+ "\nGanador: " + ganador.getNombre());
+        }
+
+    }
+
+    public int getPuntaje1() {
+        return puntaje1;
+    }
+
+    public void setPuntaje1(int puntaje1) {
+        this.puntaje1 = puntaje1;
+    }
+
+    public int getPuntaje2() {
+        return puntaje2;
+    }
+
+    public void setPuntaje2(int puntaje2) {
+        this.puntaje2 = puntaje2;
+    }
+
+    public Participante getGanador() {
+        return ganador;
+    }
+    public EstadoPartida getEstado() {
+        return estado;
+    }
 }
