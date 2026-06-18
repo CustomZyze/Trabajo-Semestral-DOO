2356c686270b3b334567b007d8b491322353b726
Matias Cabello <maticab170@gmail.com>
2026-06-18 03:03:50 -0400

    feat: Crear eliminacion directo y implementar metodo para avanzar en rondas

--- src/main/java/Formato.java
+++ src/main/java/Formato.java
@@ -2,4 +2,5 @@ import java.util.List;
 
 public interface Formato {
     List<Partida> generarCalendario(List<Participante> participantes);
+    List<Partida> avanzarRonda(List<Partida> rondaActual);
 }
