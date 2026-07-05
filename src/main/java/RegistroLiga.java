3be274775d4cfe51b4efa4283dbfc6e69421cf4a
Paulo <paulotorrescavila@gmail.com>
2026-07-04 21:35:12 -0400

    feat: Agregar clase RegistroLiga como registro de estadisticas para LigaSimple

---
+++ src/main/java/RegistroLiga.java
@@ -0,0 +1,59 @@
+public class RegistroLiga {
+    private Participante participante;
+    private int puntos;
+    private int partidosJugados;
+    private int ganados;
+    private int empatados;
+    private int perdidos;
+
+    public RegistroLiga(Participante participante){
+        this.participante = participante;
+        this.puntos = 0;
+        this.partidosJugados = 0;
+        this.ganados = 0;
+        this.empatados = 0;
+        this.perdidos = 0;
+    }
+    public void registrarVictoria(){
+        partidosJugados++;
+        ganados++;
+        puntos += 3;
+    }
+
+    public void registrarDerrota(){
+        partidosJugados++;
+        perdidos++;
+        puntos += 0;
+    }
+
+    public void registrarEmpate(){
+        partidosJugados++;
+        empatados++;
+        puntos += 1;
+    }
+
+    public int getPuntos(){
+        return puntos;
+    }
+
+    public int getPartidosJugados(){
+        return partidosJugados;
+    }
+
+    public int getGanados(){
+        return ganados;
+    }
+
+    public int getPerdidos(){
+        return perdidos;
+    }
+
+    public int getEmpatados(){
+        return empatados;
+    }
+
+    public Participante getParticipante(){
+        return participante;
+    }
+
+}
