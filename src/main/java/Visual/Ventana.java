91e57bea7385540f57e5e549b0d4cff032c2fc14
Maximiliano Pinto <mpinto2025@udec.cl>
2026-07-04 23:14:34 -0400

    feat: crear metodo ventana

--- src/main/java/Visual/Ventana.java
+++ src/main/java/Visual/Ventana.java
@@ -1,4 +1,30 @@
 package Visual;
+import LogicaTorneo.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class Ventana {
+public class Ventana extends JFrame {
+    private JPanel contenedor;
+    private CardLayout cardLayout;
+    private Torneo torneo;
+
+    public Ventana() {
+        setTitle("Sistema de Torneos");
+        setSize(900, 650);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        cardLayout = new CardLayout();
+        contenedor = new JPanel(cardLayout);
+
+        // agregar paneles
+        contenedor.add(new PanelMenu(this), "MENU");
+        contenedor.add(new PanelCrearTorneo(this), "CREAR");
+
+        add(contenedor, BorderLayout.CENTER);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        cardLayout.show(contenedor, "MENU");
+    }
 }
